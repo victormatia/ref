f66ba984cc00418855aee7b026cc3622a7dc592f
victormatia <fvictor1705@gmail.com>
2025-12-26 14:36:23 -0300

    build: arquivos de configuração

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,13 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
   ...compat.extends(
@@ -46,6 +46,6 @@ const eslintConfig = [
       'jsx-a11y/role-supports-aria-props': 'warn',
     },
   },
-]
+];
 
-export default eslintConfig
+export default eslintConfig;
